8743d057e4899193a49da9244b3763fae18681b9
Christian Heike <Christian.Heike@icloud.com>
2018-05-22 14:12:12 +0200

    another test, getting excited, yaay, not.

--- src/main/java/ta/nemahuta/neo4j/structure/SoftRefMap.java
+++ src/main/java/ta/nemahuta/neo4j/structure/SoftRefMap.java
@@ -4,10 +4,10 @@ import lombok.NonNull;
 
 import javax.annotation.Nonnull;
 import java.lang.ref.SoftReference;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.function.Supplier;
 
 /**
@@ -18,7 +18,15 @@ import java.util.function.Supplier;
  */
 public class SoftRefMap<K, V> {
 
-    private final ConcurrentMap<K, SoftReference<V>> wrapped = new ConcurrentHashMap<>();
+    private final Map<K, SoftReference<V>> wrapped;
+
+    public SoftRefMap() {
+        this(new ConcurrentHashMap<>());
+    }
+
+    protected SoftRefMap(@Nonnull @NonNull final Map<K, SoftReference<V>> wrapped) {
+        this.wrapped = wrapped;
+    }
 
     /**
      * Get or create an entry for the key.
---
+++ src/test/java/ta/nemahuta/neo4j/structure/SoftRefMapTest.java
@@ -0,0 +1,36 @@
+package ta.nemahuta.neo4j.structure;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.ref.SoftReference;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class SoftRefMapTest {
+
+    private final Map<String, SoftReference<String>> internalMap = new HashMap<>();
+    private final SoftRefMap<String, String> sut = new SoftRefMap<>(internalMap);
+
+    @Test
+    void getOrCreateForNonExistingKey() {
+        // when: 'requesting an entry which is not present'
+        assertEquals("B", sut.getOrCreate("A", () -> "B"));
+        // then: 'a soft reference ist stored for it'
+        assertNotNull(internalMap.get("A"));
+    }
+
+    @Test
+    void getOrCreateForNullSoftReference() {
+        // setup: 'an entry which is removed'
+        internalMap.put("A", new SoftReference<>(null));
+        // when: 'requesting an entry which is present but null'
+        assertEquals("B", sut.getOrCreate("A", () -> "B"));
+        // then: 'a soft reference ist stored for it'
+        assertNotNull(internalMap.get("A"));
+    }
+
+
+}
