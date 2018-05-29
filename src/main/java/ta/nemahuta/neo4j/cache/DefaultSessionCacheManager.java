45f6ccae4b9a990f7eb951f9b1cf4663e8816b37
Christian Heike <Christian.Heike@icloud.com>
2018-05-29 14:06:13 +0200

    That file was not to be committed: fix => no expiry for session cache

--- src/main/java/ta/nemahuta/neo4j/cache/DefaultSessionCacheManager.java
+++ src/main/java/ta/nemahuta/neo4j/cache/DefaultSessionCacheManager.java
@@ -6,7 +6,6 @@ import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.units.MemoryUnit;
-import org.ehcache.expiry.ExpiryPolicy;
 import ta.nemahuta.neo4j.state.Neo4JEdgeState;
 import ta.nemahuta.neo4j.state.Neo4JVertexState;
 
@@ -24,30 +23,30 @@ public class DefaultSessionCacheManager implements SessionCacheManager {
 
     public DefaultSessionCacheManager(@Nonnull final CacheManager cacheManager) {
         this.cacheManager = cacheManager;
-        this.globalEdgeCache = createEdgeCache(CACHE_NAME_EDGE_GLOBAL, ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(30)));
-        this.globalVertexCache = createVertexCache(CACHE_NAME_VERTEX_GLOBAL, ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(30)));
+        this.globalEdgeCache = createEdgeCache(CACHE_NAME_EDGE_GLOBAL);
+        this.globalVertexCache = createVertexCache(CACHE_NAME_VERTEX_GLOBAL);
     }
 
-    private Cache<Long, Neo4JVertexState> createVertexCache(final String name, final ExpiryPolicy<Object, Object> expiry) {
+    private Cache<Long, Neo4JVertexState> createVertexCache(final String name) {
         return cacheManager.createCache(name,
                 CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, Neo4JVertexState.class,
                         ResourcePoolsBuilder.newResourcePoolsBuilder().heap(200, MemoryUnit.MB))
-                        .withExpiry(expiry));
+                        .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(30))));
     }
 
-    protected Cache<Long, Neo4JEdgeState> createEdgeCache(final String name, final ExpiryPolicy<Object, Object> expiry) {
+    protected Cache<Long, Neo4JEdgeState> createEdgeCache(final String name) {
         return cacheManager.createCache(name,
                 CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, Neo4JEdgeState.class,
                         ResourcePoolsBuilder.newResourcePoolsBuilder().heap(200, MemoryUnit.MB))
-                        .withExpiry(expiry));
+                        .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofMinutes(30))));
     }
 
     @Override
     public SessionCache createSessionCache(final Object id) {
         final String vertexCacheName = "vertex-session-" + id;
         final String edgeCacheName = "edge-session-" + id;
-        final Cache<Long, Neo4JVertexState> sessionVertexCache = createVertexCache(vertexCacheName, ExpiryPolicyBuilder.noExpiration());
-        final Cache<Long, Neo4JEdgeState> sessionEdgeCache = createEdgeCache(edgeCacheName, ExpiryPolicyBuilder.noExpiration());
+        final Cache<Long, Neo4JVertexState> sessionVertexCache = createVertexCache(vertexCacheName);
+        final Cache<Long, Neo4JEdgeState> sessionEdgeCache = createEdgeCache(edgeCacheName);
         return new DefaultSessionCache(
                 new DefaultHierarchicalCache<>(globalEdgeCache, sessionEdgeCache),
                 new DefaultHierarchicalCache<>(globalVertexCache, sessionVertexCache)) {
